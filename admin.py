fd9571c0dd422b17f42eabf1d82403514aee475b
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-05-06 17:18:59 +1000

    added admin.py

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.apps import apps
+
+# add your app name here
+app = apps.get_app_config("")
+
+for model_name, model in app.models.items():
+    exclude = []
+    if model_name not in exclude:
+        admin.site.register(model)
+
